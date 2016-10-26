dbe779fb6eb0a50a7d695df52dc848284820d922
Jonathan Karr <jonrkarr@gmail.com>
2016-10-26 11:57:00 -0400

    debugging setup

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
 from setuptools import setup, find_packages
-import unitth
 import os
+import re
+import unitth
 
 # parse dependencies and their links from requirements.txt files
-install_requires = [line.rstrip() for line in open('requirements.txt')]
-tests_require = [line.rstrip() for line in open('tests/requirements.txt')]
+install_requires = []
+tests_require = []
+dependency_links = []
+
+for line in open('requirements.txt'):
+    pkg_src = line.rstrip()
+    match = re.match('^.+#egg=(.*?)$', pkg_src)
+    if match:
+        pkg_id = match.group(1)
+        dependency_links.append(pkg_src)
+    else:
+        pkg_id = pkg_src
+    install_requires.append(pkg_id)
+
+for line in open('tests/requirements.txt'):
+    pkg_src = line.rstrip()
+    match = re.match('^.+#egg=(.*?)$', pkg_src)
+    if match:
+        pkg_id = match.group(1)
+        dependency_links.append(pkg_src)
+    else:
+        pkg_id = pkg_src
+    tests_require.append(pkg_id)
+dependency_links = list(set(dependency_links))
 
 # install package
 setup(
@@ -23,6 +46,7 @@ setup(
     },
     install_requires=install_requires,
     tests_require=tests_require,
+    dependency_links=dependency_links,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
